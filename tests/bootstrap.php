e75ca4b4fcb6722d8f27ef0316160433672f5dba
c9s <cornelius.howl@gmail.com>
2013-03-15 21:09:14 +0800

    Fix require path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 require 'PHPUnit/TestMore.php';
+require 'PHPUnit/Framework/ExtensionTestCase.php';
 require 'Universal/ClassLoader/BasePathClassLoader.php';
-require '/Users/c9s/git/Work/PHPUnit_Framework_ExtensionTestCase/src/PHPUnit/Framework/ExtensionTestCase.php';
 
 define('BASEDIR',dirname(dirname(__FILE__)));
 $classLoader = new \Universal\ClassLoader\BasePathClassLoader(array( 
