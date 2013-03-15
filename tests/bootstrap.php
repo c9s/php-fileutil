a14e4414b1f82eb217f19e555784b3e702c9c976
c9s <cornelius.howl@gmail.com>
2013-03-15 21:18:12 +0800

    use debug flag for phpunit testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 require 'PHPUnit/TestMore.php';
-require 'PHPUnit/Framework/ExtensionTestCase.php';
+// require 'PHPUnit/Framework/ExtensionTestCase.php';
+require '../PHPUnit_Framework_ExtensionTestCase/src/PHPUnit/Framework/ExtensionTestCase.php';
 require 'Universal/ClassLoader/BasePathClassLoader.php';
 
-define('BASEDIR',dirname(dirname(__FILE__)));
+if ( !defined('BASEDIR') ) {
+    define('BASEDIR',dirname(dirname(__FILE__)));
+}
 $classLoader = new \Universal\ClassLoader\BasePathClassLoader(array( 
     BASEDIR . '/src',
     BASEDIR . '/vendor/pear',
