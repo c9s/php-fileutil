6da4531bf2f2140fe2edeba7804b9781294ba808
c9s <cornelius.howl@gmail.com>
2013-03-17 15:11:18 +0800

    use reference

--- src/FileUtil.php
+++ src/FileUtil.php
@@ -120,17 +120,19 @@ function futil_rmtree($path)
 }
 
 
-function futil_paths_append($paths,$path2)
+function futil_paths_append(& $paths,$path2)
 {
-    foreach( $paths as & $path ) {
-        $path = futil_pathjoin($path ,$path2);
+    $count = count($paths);
+    for($i = 0 ; $i < $count ; $i++ ) {
+        $paths[$i] = futil_pathjoin($paths[$i] ,$path2);
     }
 }
 
-function futil_paths_prepend($paths,$path2)
+function futil_paths_prepend(& $paths,$path2)
 {
-    foreach( $paths as & $path ) {
-        $path = futil_pathjoin($path2 ,$path);
+    $count = count($paths);
+    for($i = 0 ; $i < $count ; $i++ ) {
+        $paths[$i] = futil_pathjoin($path2, $paths[$i]);
     }
 }
 
