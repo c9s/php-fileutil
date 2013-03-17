934acf3f83470383dab2fc4e2deaa7b4ffd5c1e2
c9s <cornelius.howl@gmail.com>
2013-03-17 16:28:50 +0800

    Move include zend header files to php_fileutil.h

--- php_fileutil.h
+++ php_fileutil.h
@@ -3,6 +3,21 @@
 #define PHP_FILEUTIL_VERSION "1.0"
 #define PHP_FILEUTIL_EXTNAME "fileutil"
 
+#include <Zend/zend.h>
+#include <Zend/zend_constants.h>
+#include <Zend/zend_execute.h>
+#include <Zend/zend_exceptions.h>
+#include <Zend/zend_hash.h>
+#include <Zend/zend_interfaces.h>
+#include <Zend/zend_operators.h>
+#include <Zend/zend_qsort.h>
+#include <Zend/zend_vm.h>
+
+#include <ext/standard/php_standard.h>
+#include <ext/standard/php_filestat.h>
+#include <ext/standard/php_string.h>
+
+
 PHP_FUNCTION(futil_scanpath);
 PHP_FUNCTION(futil_scanpath_dir);
 PHP_FUNCTION(futil_pathjoin);
@@ -16,10 +31,23 @@ PHP_FUNCTION(futil_rmdir_if_exists);
 PHP_FUNCTION(futil_mkdir_if_not_exists);
 PHP_FUNCTION(futil_rmtree);
 
+
+#if HAVE_SPL
+#include <ext/spl/spl_array.h>
+#include <ext/spl/spl_directory.h>
+#include <ext/spl/spl_engine.h>
+#include <ext/spl/spl_exceptions.h>
+#include <ext/spl/spl_iterators.h>
+#endif
+
 #ifndef bool
 #define bool short
 #define false 0
 #define true 1
+#endif
+
+#ifndef tsrm_ls
+#define tsrm_ls
 #endif
 
 bool futil_stream_is_dir(php_stream *stream TSRMLS_DC);
